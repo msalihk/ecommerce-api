c2597c092b2961d1ac02ea9f29e1ec1e3b9d1bc4
Salih Küçükkayış <salih@storespy.net>
2023-01-23 21:49:48 +0300

    Added models and migrations

---
+++ database/migrations/2023_01_23_144544_create_adresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->foreignId('district_id');
+            $table->foreignId('city_id');
+            $table->longText('line_1');
+            $table->longText('line_2');
+            $table->integer('phone');
+            $table->integer('postal_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+};
---
+++ database/migrations/2023_01_23_150442_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('SKU')->unique();
+            $table->integer('stock');
+            $table->string('image')->default('http://imageurl.com');
+            $table->text('description');
+            $table->boolean('status');
+            $table->foreignId('category_id');
+            $table->foreignId('brand_id');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
